25e1bef3046771cc6cd7634d9111f60a56974073
Jonathan Heathcote <mail@jhnet.co.uk>
2015-04-05 22:20:00 +0100

    Nearly working: ordering of Bs and Cs lost when AA.
    Could append this info to the A too. Now you have info loss on two As both
    having this info in it which doesn't match...

--- arrow_of_time/arrow_of_time.py
+++ arrow_of_time/arrow_of_time.py
@@ -15,22 +15,25 @@ C = "C"
 
 class Particle(object):
 	"""A single particle."""
-	def __init__(self, type=A, x=0.0, y=0.0, vx=0.0, vy=0.0):
+	def __init__(self, type=A, x=0.0, y=0.0, vx=0.0, vy=0.0, aux=None):
 		self.type = type
 		self.x = x
 		self.y = y
 		self.vx = vx
 		self.vy = vy
+		self.aux = aux
 
 
 class Universe(object):
 	"""A model of the universe where there are two fundamental particle types.
 	
 	Particles of type A aniahlate upon collision producing two particles of type B
-	and C which have the same velocities of the two A particles.
+	and one of type C. The Bs have same velocities of the two A particles while
+	C's velocity is composed of the aux values of the two A particles.
 	
-	If a B and C particle collide, two type A particles are produced, taking the
-	same velocities again.
+	If two Bs and C particle collide, two type A particles are produced, taking the
+	same velocities as the Bs and aux values from the velocity components of the C
+	particle's vector.
 	"""
 	
 	def __init__(self, width, height):
@@ -81,7 +84,11 @@ class Universe(object):
 				ps.remove(a2)
 				
 				a1.type = B
-				a2.type = C
+				a2.type = B
+				c = Particle(C, a1.x, a1.y, a1.aux, a2.aux, None)
+				a1.aux = None
+				a2.aux = None
+				self.particles.insert(self.particles.index(a2) + 1, c)
 	
 	
 	def _collide_bc(self, grid, step):
@@ -93,12 +100,19 @@ class Universe(object):
 			b_iter = iter(reversed(b_particles))
 			c_iter = iter(reversed(c_particles))
 			
-			for b, c in zip(b_iter, c_iter):
-				ps.remove(b)
+			for b1, b2, c in zip(b_iter, b_iter, c_iter):
+				ps.remove(b1)
+				ps.remove(b2)
 				ps.remove(c)
 				
-				b.type = A
-				c.type = A
+				b1.type = A
+				b2.type = A
+				
+				# Other way around(!)
+				b1.aux = c.vy
+				b2.aux = c.vx
+				
+				self.particles.remove(c)
 	
 	def __str__(self):
 		return ", ".join("{}({}, {})".format(p.type, p.x, p.y) for p in u.particles)
@@ -110,11 +124,14 @@ def make_random_universe(width, height, n_particles, types):
 	"""
 	u = Universe(width, height)
 	for n in range(n_particles):
-		u.particles.append(Particle(type=random.choice(types),
+		type = random.choice(types)
+		u.particles.append(Particle(type=type,
 		                            x=random.randint(0, width-1),
 		                            y=random.randint(0, height-1),
 		                            vx=random.randint(0, width-1),
-		                            vy=random.randint(0, height-1)))
+		                            vy=random.randint(0, height-1),
+		                            aux=random.randint(0, min(width, height)-1)
+		                                if type == A else None))
 	return u
 
 
--- arrow_of_time/test_arrow_of_time.py
+++ arrow_of_time/test_arrow_of_time.py
@@ -6,7 +6,7 @@ from arrow_of_time import A, B, C, Particle, Universe
 
 
 def dump_state(u):
-	return [(p.type, p.x, p.y, p.vx, p.vy) for p in u.particles]
+	return [(p.type, p.x, p.y, p.vx, p.vy, p.aux) for p in u.particles]
 
 
 def test_velocity():
@@ -51,39 +51,12 @@ def test_wrap():
 
 def test_collide_aa():
 	u = Universe(10, 10)
-	u.particles.append(Particle(A, 0, 0, 1, 0))
-	u.particles.append(Particle(A, 2, 0, -1, 0))
+	u.particles.append(Particle(A, 0, 0, 1, 0, 2))
+	u.particles.append(Particle(A, 2, 0, -1, 0, 3))
 	
 	before = dump_state(u)
 	
 	# Check collision occurs correctly
-	u.step()
-	assert len(u.particles) == 2
-	assert u.particles[0].type == B
-	assert u.particles[0].x == 1
-	assert u.particles[0].y == 0
-	assert u.particles[0].vx == 1
-	assert u.particles[0].vy == 0
-	
-	assert u.particles[1].type == C
-	assert u.particles[1].x == 1
-	assert u.particles[1].y == 0
-	assert u.particles[1].vx == -1
-	assert u.particles[1].vy == 0
-	
-	u.step(-1)
-	assert before == dump_state(u)
-
-
-def test_collide_aaa():
-	u = Universe(10, 10)
-	u.particles.append(Particle(A, 0, 0, 1, 0))
-	u.particles.append(Particle(A, 2, 0, -1, 0))
-	u.particles.append(Particle(A, 1, 1, 0, -1))
-	
-	before = dump_state(u)
-	
-	# Check collision occurs correctly and that third particle remains an A
 	u.step()
 	assert len(u.particles) == 3
 	assert u.particles[0].type == B
@@ -91,29 +64,31 @@ def test_collide_aaa():
 	assert u.particles[0].y == 0
 	assert u.particles[0].vx == 1
 	assert u.particles[0].vy == 0
+	assert u.particles[0].aux == None
 	
-	assert u.particles[1].type == C
+	assert u.particles[1].type == B
 	assert u.particles[1].x == 1
 	assert u.particles[1].y == 0
 	assert u.particles[1].vx == -1
 	assert u.particles[1].vy == 0
+	assert u.particles[1].aux == None
 	
-	assert u.particles[2].type == A
+	assert u.particles[2].type == C
 	assert u.particles[2].x == 1
 	assert u.particles[2].y == 0
-	assert u.particles[2].vx == 0
-	assert u.particles[2].vy == -1
+	assert u.particles[2].vx == 2
+	assert u.particles[2].vy == 3
+	assert u.particles[2].aux == None
 	
 	u.step(-1)
 	assert before == dump_state(u)
 
 
-def test_collide_aaaa():
+def test_collide_aaa():
 	u = Universe(10, 10)
-	u.particles.append(Particle(A, 0, 0, 1, 0))
-	u.particles.append(Particle(A, 2, 0, -1, 0))
-	u.particles.append(Particle(A, 1, 1, 0, -1))
-	u.particles.append(Particle(A, 1, 9, 0, 1))
+	u.particles.append(Particle(A, 0, 0, 1, 0, 2))
+	u.particles.append(Particle(A, 2, 0, -1, 0, 3))
+	u.particles.append(Particle(A, 1, 1, 0, -1, 4))
 	
 	before = dump_state(u)
 	
@@ -125,34 +100,97 @@ def test_collide_aaaa():
 	assert u.particles[0].y == 0
 	assert u.particles[0].vx == 1
 	assert u.particles[0].vy == 0
+	assert u.particles[0].aux == None
 	
-	assert u.particles[1].type == C
+	assert u.particles[1].type == B
 	assert u.particles[1].x == 1
 	assert u.particles[1].y == 0
 	assert u.particles[1].vx == -1
 	assert u.particles[1].vy == 0
+	assert u.particles[1].aux == None
 	
-	assert u.particles[2].type == B
+	assert u.particles[2].type == C
 	assert u.particles[2].x == 1
 	assert u.particles[2].y == 0
-	assert u.particles[2].vx == 0
-	assert u.particles[2].vy == -1
+	assert u.particles[2].vx == 2
+	assert u.particles[2].vy == 3
+	assert u.particles[2].aux == None
 	
-	assert u.particles[3].type == C
+	assert u.particles[3].type == A
 	assert u.particles[3].x == 1
 	assert u.particles[3].y == 0
 	assert u.particles[3].vx == 0
-	assert u.particles[3].vy == 1
+	assert u.particles[3].vy == -1
+	assert u.particles[3].aux == 4
 	
 	u.step(-1)
 	assert before == dump_state(u)
 
 
-@pytest.mark.parametrize("p1,p2", [(B, C), (C, B)])
-def test_collide_bc(p1, p2):
+def test_collide_aaaa():
 	u = Universe(10, 10)
-	u.particles.append(Particle(p1, 0, 0, 1, 0))
-	u.particles.append(Particle(p2, 2, 0, -1, 0))
+	u.particles.append(Particle(A, 0, 0, 1, 0, 2))
+	u.particles.append(Particle(A, 2, 0, -1, 0, 3))
+	u.particles.append(Particle(A, 1, 1, 0, -1, 4))
+	u.particles.append(Particle(A, 1, 9, 0, 1, 5))
+	
+	before = dump_state(u)
+	
+	# Check collision occurs correctly and that third particle remains an A
+	u.step()
+	assert len(u.particles) == 6
+	assert u.particles[0].type == B
+	assert u.particles[0].x == 1
+	assert u.particles[0].y == 0
+	assert u.particles[0].vx == 1
+	assert u.particles[0].vy == 0
+	assert u.particles[0].aux == None
+	
+	assert u.particles[1].type == B
+	assert u.particles[1].x == 1
+	assert u.particles[1].y == 0
+	assert u.particles[1].vx == -1
+	assert u.particles[1].vy == 0
+	assert u.particles[1].aux == None
+	
+	assert u.particles[2].type == C
+	assert u.particles[2].x == 1
+	assert u.particles[2].y == 0
+	assert u.particles[2].vx == 2
+	assert u.particles[2].vy == 3
+	assert u.particles[2].aux == None
+	
+	assert u.particles[3].type == B
+	assert u.particles[3].x == 1
+	assert u.particles[3].y == 0
+	assert u.particles[3].vx == 0
+	assert u.particles[3].vy == -1
+	assert u.particles[3].aux == None
+	
+	assert u.particles[4].type == B
+	assert u.particles[4].x == 1
+	assert u.particles[4].y == 0
+	assert u.particles[4].vx == 0
+	assert u.particles[4].vy == 1
+	assert u.particles[4].aux == None
+	
+	assert u.particles[5].type == C
+	assert u.particles[5].x == 1
+	assert u.particles[5].y == 0
+	assert u.particles[5].vx == 4
+	assert u.particles[5].vy == 5
+	assert u.particles[5].aux == None
+	
+	
+	u.step(-1)
+	assert before == dump_state(u)
+
+
+def test_collide_bc():
+	u = Universe(10, 10)
+	u.particles.append(Particle(B, 0, 0, 1, 0))
+	u.particles.append(Particle(B, 2, 0, -1, 0))
+	u.particles.append(Particle(C, 3, 1, -2, -1))
 	
 	before = dump_state(u)
 	
@@ -164,16 +202,15 @@ def test_collide_bc(p1, p2):
 	assert u.particles[0].y == 0
 	assert u.particles[0].vx == 1
 	assert u.particles[0].vy == 0
+	assert u.particles[0].aux == -2
 	
 	assert u.particles[1].type == A
 	assert u.particles[1].x == 1
 	assert u.particles[1].y == 0
 	assert u.particles[1].vx == -1
 	assert u.particles[1].vy == 0
+	assert u.particles[1].aux == -1
 	
-	print(before)
-	print(dump_state(u))
 	u.step(-1)
-	print(dump_state(u))
 	assert before == dump_state(u)
 	
